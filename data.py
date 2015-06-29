Read-in data transfered to fixed dimension order, distance & correlation computation added

data.py
@@ -1,76 +1,134 @@
 #! /usr/bin/env python
 from eofs.standard import Eof
+from netCDF4 import Dataset
 import numpy as np
-from numpy import mean
+from numpy import mean,sin,cos,arccos, pi
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
-class Data:
-	def __init__(self,data,time,lon,lat):
-		self._data=np.array(data)
-		self._time=time
-		self._lon=np.array(lon)
-		self._lat=np.array(lat)
-		print self._time[0]-self._time[1]
-		return
-	def filter(self,key,arg=None):
-		if key=='period':		#in days
-			period=arg
-			data_per_period=period*24/(self._time[1]-self._time[0])
-			new_len=int(len(self._time)//data_per_period)
-			self._time=np.array(map(lambda x: self._time[x*data_per_period],range(new_len)))
-			data=self._data[:]
-			self._data=np.zeros([len(self._time),len(self._lat),len(self._lon)])
-			for i,lat in enumerate(self._lat):
-				for j,lon  in enumerate(self._lon):
-					self._data[:,i,j]=np.array(map(lambda x: np.mean(data[x*period:(x+1)*period,i,j]),range(new_len)))
-		return self._data
-	def eof(self,no_of_eofs=1):
-		data_mean = self._data.mean(axis=0)
-		coslat = np.cos(np.deg2rad(self._lat)).clip(0., 1.) 	#weighting
-		wgts = np.sqrt(coslat)[..., np.newaxis]
-		solver = Eof(self._data, weights=wgts)
-		# Retrieve the leading EOF, expressed as the covariance between the leading PC
-		# time series and the input SLP anomalies at each grid point.
-		eof = solver.eofsAsCorrelation(neofs=no_of_eofs)
-		fraction=solver.varianceFraction(no_of_eofs)
-		return Eof_pattern(eof, fraction,self._lat,self._lon)
-class Eof_pattern:
-	def __init__(self,eof,fraction,lat,lon):
-		self._eof=eof
-		self._fraction=fraction
-		self._lat=lat
-		self._lon=lon
-	def plot_eof(self,name,no_eofs=1):
-		m = Basemap(projection='ortho', lat_0=-60., lon_0=100.)#todo
-		world=[0,-90,360,0]
-		min_lat=self._lat[0]
-		max_lat=self._lat[-1]
-		min_lon=self._lon[0]
-		max_lon=self._lon[-1]
-		dif_lat=(max_lat-min_lat)
-		if max_lon%360==min_lon%360:
-			dif_lon=360
-		else:	
-			dif_lon=(max_lon-min_lon)%360
-		lon_line=5
-		while dif_lon/lon_line>9:
-			lon_line+=5
-		lat_line=5
-		while dif_lat/lat_line>10:
-			lat_line+=5
-		for i in range(no_eofs):
-			plt.figure(i)
-			m=Basemap(projection='gall',llcrnrlat=min_lat,urcrnrlat=max_lat,\
-			    llcrnrlon=min_lon,urcrnrlon=max_lon,resolution='c')
+from progressbar import ProgressBar
 
-			x, y = m(*np.meshgrid(self._lon, self._lat))
-			levels = np.linspace(-1,1,11)
-			m.contourf(x, y, self._eof[i].squeeze(),levels, cmap=plt.cm.RdBu_r)
-			m.drawcoastlines()
-			m.drawparallels(np.arange(-80.,86.,lat_line),labels=[True,False,False,False])
-			m.drawmeridians(np.arange(-180.,181.,lon_line),labels=[False,False,False,True])
-			plt.title("EOF ("+"%0.1f"%(self._fraction[i]*100)+"%)" , fontsize=16)
-			cb = plt.colorbar(orientation='horizontal')
-			cb.set_label('correlation coefficient', fontsize=12)
-		plt.savefig(name+'.pdf',figsize=(15, 6), dpi=300, facecolor='w', edgecolor='w')
-		plt.clf()
+#------------------------------------#
+#Class for the data processing
+#------------------------------------#
+
+class Data:
+    def __init__(self,data,time,lon,lat):
+        self._data=np.array(data)
+        self._time=time
+        self._lon=np.array(lon)
+        self._lat=np.array(lat)
+        return
+    def filter(self,key,arg=None):
+        if key=='period':		#in days
+            period=arg
+            data_per_period=period*24/(self._time[1]-self._time[0])
+            new_len=int(len(self._time)//data_per_period)
+            self._time=np.array(map(lambda x: self._time[x*data_per_period],range(new_len)))
+            data=self._data[:]
+            self._data=np.zeros([len(self._time),len(self._lat),len(self._lon)])
+            for i,lat in enumerate(self._lat):
+                for j,lon  in enumerate(self._lon):
+                    self._data[:,i,j]=np.array(map(lambda x: np.mean(data[x*period:(x+1)*period,i,j]),range(new_len)))
+        return self._data
+    def eof(self,no_of_eofs=1):
+        data_mean = self._data.mean(axis=0)
+        coslat = np.cos(np.deg2rad(self._lat)).clip(0., 1.) 	#weighting
+        wgts = np.sqrt(coslat)[..., np.newaxis]
+        solver = Eof(self._data, weights=wgts)
+        # Retrieve the leading EOF, expressed as the covariance between the leading PC
+        # time series and the input SLP anomalies at each grid point.
+        eof = solver.eofsAsCorrelation(neofs=no_of_eofs)
+        fraction=solver.varianceFraction(no_of_eofs)
+        return Eof_pattern(eof, fraction,self._lat,self._lon)
+    def compute_correlation(self,file_name):
+        corfile = Dataset(file_name, 'w', format='NETCDF4')
+        corfile.createDimension('lat', len(self._lat))
+        corfile.createDimension('lat_mir',len(self._lat))
+        corfile.createDimension('lon', len(self._lon))
+        corfile.createDimension('lon_mir', len(self._lon))
+        lat=corfile.createVariable('latidude','f4',('lat',))
+        lat_mir=corfile.createVariable('latidude_mirror','f4',('lat_mir',))
+        lon=corfile.createVariable('longitude','f4',('lon',))
+        lon_mir=corfile.createVariable('longitude_mirror','f4',('lon_mir',))
+        cor=corfile.createVariable('cor','f4',('lat','lon','lat_mir','lon_mir',))
+        lat[:],lat_mir[:]=self._lat,self._lat
+        lon[:], lon_mir[:] =self._lon, self._lon
+        progress = ProgressBar(maxval=len(self._lon))
+        print "progress"
+        for i_lon,lon in progress(enumerate(self._lon)):
+            progress.update(i_lon)
+            for i_lat,lat in enumerate(self._lat):
+                for i_lon_mir,lon_mir in enumerate(self._lon):
+                    for i_lat_mir,lat_mir in enumerate(self._lat):
+                        cor[i_lat,i_lon,i_lat_mir,i_lon_mir]=np.corrcoef(self._data[:,i_lat,i_lon],self._data[:,i_lat_mir,i_lon_mir])[0,1]
+        corfile.close()
+    @staticmethod
+    def __great_circle_distance(lat0,lon0,lat1,lon1):
+        d2r=pi/180#degreees to radient factor
+        dist_norm= arccos(sin(d2r*lat0)*sin(d2r*lat1)+cos(d2r*lat0)*cos(d2r*lat1)*cos(d2r*abs(lon0-lon1)))
+        return  6371*dist_norm
+    def compute_distances(self,file_name):
+        distfile = Dataset(file_name, 'w', format='NETCDF4')
+        distfile.createDimension('lat', len(self._lat))
+        distfile.createDimension('lat_mir',len(self._lat))
+        distfile.createDimension('lon', len(self._lon))
+        distfile.createDimension('lon_mir', len(self._lon))
+        lat=distfile.createVariable('latidude','f4',('lat',))
+        lat_mir=distfile.createVariable('latidude_mirror','f4',('lat_mir',))
+        lon=distfile.createVariable('longitude','f4',('lon',))
+        lon_mir=distfile.createVariable('longitude_mirror','f4',('lon_mir',))
+        dist=distfile.createVariable('dist','f4',('lat','lon','lat_mir','lon_mir',))
+        lat[:],lat_mir[:]=self._lat,self._lat
+        lon[:], lon_mir[:] =self._lon, self._lon
+        progress = ProgressBar(maxval=len(self._lon))
+        print "progress"
+        for i_lon,lon in progress(enumerate(self._lon)):
+            progress.update(i_lon)
+            for i_lat,lat in enumerate(self._lat):
+                for i_lon_mir,lon_mir in enumerate(self._lon):
+                    for i_lat_mir,lat_mir in enumerate(self._lat):
+                        dist[i_lat,i_lon,i_lat_mir,i_lon_mir]=self.__great_circle_distance(i_lat,i_lon,i_lat_mir,i_lon_mir)
+        distfile.close()
+    def analyse_correlation(self, filename):
+        corfile = Dataset(file_name, 'r', format='NETCDF4')
+        
+class Eof_pattern:
+    def __init__(self,eof,fraction,lat,lon):
+        self._eof=eof
+        self._fraction=fraction
+        self._lat=lat
+        self._lon=lon
+    def plot_eof(self,name,no_eofs=1):
+        m = Basemap(projection='ortho', lat_0=-60., lon_0=100.)#todo
+        world=[0,-90,360,0]
+        min_lat=self._lat[0]
+        max_lat=self._lat[-1]
+        min_lon=self._lon[0]
+        max_lon=self._lon[-1]
+        dif_lat=(max_lat-min_lat)
+        if max_lon%360==min_lon%360:
+            dif_lon=360
+        else:	
+            dif_lon=(max_lon-min_lon)%360
+        lon_line=5
+        while dif_lon/lon_line>9:
+            lon_line+=5
+        lat_line=5
+        while dif_lat/lat_line>10:
+            lat_line+=5
+        for i in range(no_eofs):
+            plt.figure(i)
+            m=Basemap(projection='gall',llcrnrlat=min_lat,urcrnrlat=max_lat,\
+                llcrnrlon=min_lon,urcrnrlon=max_lon,resolution='c')
+
+            x, y = m(*np.meshgrid(self._lon, self._lat))
+            levels = np.linspace(-1,1,11)
+            m.contourf(x, y, self._eof[i].squeeze(),levels, cmap=plt.cm.RdBu_r)
+            m.drawcoastlines()
+            m.drawparallels(np.arange(-80.,86.,lat_line),labels=[True,False,False,False])
+            m.drawmeridians(np.arange(-180.,181.,lon_line),labels=[False,False,False,True])
+            plt.title("EOF ("+"%0.1f"%(self._fraction[i]*100)+"%)" , fontsize=16)
+            cb = plt.colorbar(orientation='horizontal')
+            cb.set_label('correlation coefficient', fontsize=12)
+        plt.savefig(name+'.pdf',figsize=(15, 6), dpi=300, facecolor='w', edgecolor='w')
+        plt.clf()

read_file.py
@@ -6,76 +6,79 @@ import calendar as cal
 from data import Data
 
 class Read_file:
-	def __init__(self,filename,arg=None):
-		self.__filename=filename
-#		self.__structure=structure
-		self.__arg=arg
-		self._cdf_conf={}
-		self._cdf_conf['dimensions']=3
-		self._cdf_conf['lat_name']='lat'
-		self._cdf_conf['lat_pos']=1
-		self._cdf_conf['lat_range']=[-90,0]
-		self._cdf_conf['lon_name']='lon'
-		self._cdf_conf['lon_pos']=2
-		self._cdf_conf['lon_range']=[0,360]
-		self._cdf_conf['time_name']='time'
-		self._cdf_conf['time_range']=[0,np.inf]
-		self._cdf_conf['time_pos']=0
-		self._cdf_conf['variable']='msl'
-		self._data=None
-		self._time=None
-		self._lon=None
-		self._lat=None
-		return		
-	def config(self,key,val):
-		self._cdf_conf[key]=val
-	def read(self,key=None):
-		ncin = Dataset(self.__filename, 'r')
-		if key is not None:
-			if key=='am_re':
-				self.__read_am_re()
-			print 'no read in specified for key %s'%key 
-		else:
-			#filtering the range of values
-			time=ncin.variables[self._cdf_conf['time_name']][:]
-			tr=self._cdf_conf['time_range']
-			time_ind=filter(lambda i: (time[i]>=tr[0])&(time[i]<=tr[1]),range(len(time)))
-			self._time=time[time_ind]
-			lons=ncin.variables[self._cdf_conf['lon_name']][:]%360
-			lonr=self._cdf_conf['lon_range']
-			lon_ind=filter(lambda i: (lons[i]>=lonr[0])&(lons[i]<=lonr[1]),range(len(lons)))
-			self._lon=lons[lon_ind]
-			lats=ncin.variables[self._cdf_conf['lat_name']][:]
-			latr=self._cdf_conf['lat_range']
-			lat_ind=filter(lambda i: (lats[i]>=latr[0])&(lats[i]<=latr[1]),range(len(lats)))
-			self._lat=lats[lat_ind]
-			if self._cdf_conf['dimensions']==3:
-				indices=range(3)#sorting the order of indices for the netcdf file
-				indices[self._cdf_conf['time_pos']]=time_ind
-				indices[self._cdf_conf['lon_pos']]=lon_ind
-				indices[self._cdf_conf['lat_pos']]=lat_ind
-				self._data=ncin.variables[self._cdf_conf['variable']][indices]
-	#		elif self._cdf_conf[dimensions]==4:
-			else:
-				print 'data structure unknown'
-			return Data( self._data, self._time, self._lon,self._lat)
-	def __read_am_re(self):
-		return
+    def __init__(self,filename,arg=None):
+        self.__filename=filename
+#	    self.__structure=structure
+        self.__arg=arg
+        self._cdf_conf={}
+        self._cdf_conf['dimensions']=3
+        self._cdf_conf['lat_name']='lat'
+        self._cdf_conf['lat_pos']=1
+        self._cdf_conf['lat_range']=[-90,0]
+        self._cdf_conf['lon_name']='lon'
+        self._cdf_conf['lon_pos']=2
+        self._cdf_conf['lon_range']=[0,360]
+        self._cdf_conf['time_name']='time'
+        self._cdf_conf['time_range']=[0,np.inf]
+        self._cdf_conf['time_pos']=0
+        self._cdf_conf['variable']='msl'
+        self._data=None
+        self._time=None
+        self._lon=None
+        self._lat=None
+        return		
+    def config(self,key,val):
+        self._cdf_conf[key]=val
+    def read(self,key=None):
+        ncin = Dataset(self.__filename, 'r')
+        if key is not None: 
+            if key=='am_re':
+                self.__read_am_re()
+            print 'no read in specified for key %s'%key 
+        else:
+            #filtering the range of values
+            time=ncin.variables[self._cdf_conf['time_name']][:]
+            tr=self._cdf_conf['time_range']
+            time_ind=filter(lambda i: (time[i]>=tr[0])&(time[i]<=tr[1]),range(len(time)))
+            self._time=time[time_ind]
+            lons=ncin.variables[self._cdf_conf['lon_name']][:]%360
+            lonr=self._cdf_conf['lon_range']
+            lon_ind=filter(lambda i: (lons[i]>=lonr[0])&(lons[i]<=lonr[1]),range(len(lons)))
+            self._lon=lons[lon_ind]
+            lats=ncin.variables[self._cdf_conf['lat_name']][:]
+            latr=self._cdf_conf['lat_range']
+            lat_ind=filter(lambda i: (lats[i]>=latr[0])&(lats[i]<=latr[1]),range(len(lats)))
+            self._lat=lats[lat_ind]
+            if self._cdf_conf['dimensions']==3:
+                indices=range(3)#sorting the order of indices for the netcdf file
+                indices[self._cdf_conf['time_pos']]=time_ind
+                indices[self._cdf_conf['lon_pos']]=lon_ind
+                indices[self._cdf_conf['lat_pos']]=lat_ind
+                self._data=ncin.variables[self._cdf_conf['variable']][indices]
+                if self._cdf_conf['time_pos']!=0:
+                    np.swapaxes(self._data,0,elf._cdf_conf['time_pos'])
+                if (not ((self._cdf_conf['lat_pos']==1)|(self._cdf_conf['lon_pos']==2))):
+                    np.swapaxes(self._data,1,2) #data(time,lat,lon)
+            else:#		elif self._cdf_conf[dimensions]==4:
+                print 'data structure unknown'
+            return Data( self._data, self._time, self._lon,self._lat)
+    def __read_am_re(self):
+        return
 if __name__ == "__main__":
-	test_file=Read_file('../Data/prmsl.1969-2000.nc')
-	test_file.config('lat_name','lat')
-	test_file.config('lat_pos',1)
-	test_file.config('lon_name','lon')
-	test_file.config('lon_pos',2)
-	test_file.config('time_name','time')
-	test_file.config('time_pos',0)
-	test_file.config('dimensions',3)
-	test_file.config('variable','prmsl')
-	#convert to utc timestamp
-	test_file.config('time_range',[cal.timegm(dt.date(1968,1,1).timetuple()),cal.timegm(dt.date(2000,1,1).timetuple())])
-	test_file.config('lat_range',[-70,-60])
-	test_file.config('lon_range',[0,10])
-	test_data=test_file.read()
-	bla=test_data.filter('period',7)
-	test_eof=test_data.eof()
-	test_eof.plot_eof(name='bar')
+    test_file=Read_file('../Data/prmsl.1969-2000.nc')
+    test_file.config('lat_name','lat')
+    test_file.config('lat_pos',1)
+    test_file.config('lon_name','lon')
+    test_file.config('lon_pos',2)
+    test_file.config('time_name','time')
+    test_file.config('time_pos',0)
+    test_file.config('dimensions',3)
+    test_file.config('variable','prmsl')
+    #convert to utc timestamp
+    test_file.config('time_range',[cal.timegm(dt.date(1968,1,1).timetuple()),cal.timegm(dt.date(2000,1,1).timetuple())])
+    test_file.config('lat_range',[-45,0])
+    test_file.config('lon_range',[0,360])
+    test_data=test_file.read()
+    bla=test_data.filter('period',7)
+    test_eof=test_data.eof()
+    test_eof.plot_eof(name='bar')